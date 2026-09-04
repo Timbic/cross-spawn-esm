5f0c5b8bc703d1a1f840360d6152e72dd02fe538
Тимур Бикмухаметов <tim.bic22@gmail.com>
2026-09-04 21:00:18 +0300

    chore: initial logic port to esm/ts

---
+++ src/enoent.ts
@@ -0,0 +1,39 @@
+import type { ChildProcess } from "node:child_process";
+import type { ParsedCommand } from "./utils.ts";
+
+const isWin = process.platform === "win32";
+
+function notFoundError(original: ParsedCommand["original"], syscall: "spawn" | "spawnSync") {
+	return Object.assign(new Error(`${syscall} ${original.command} ENOENT`), {
+		code: "ENOENT",
+		errno: "ENOENT",
+		syscall: `${syscall} ${original.command}`,
+		path: original.command,
+		spawnargs: original.args,
+	});
+}
+
+function verifyENOENT(status: number | null, parsed: ParsedCommand, syscall: "spawn" | "spawnSync") {
+	if (isWin && status === 1 && !parsed.file) {
+		return notFoundError(parsed.original, syscall);
+	}
+
+	return null;
+}
+
+function hookChildProcess(cp: ChildProcess, parsed: ParsedCommand) {
+	if (!isWin) return;
+
+	const originalEmit = cp.emit;
+	cp.emit = function (name: string | symbol, ...args: unknown[]) {
+		if (name === "exit") {
+			const status = args[0] as number | null;
+			const err = verifyENOENT(status, parsed, "spawn");
+			if (err) return originalEmit.call(cp, "error", err);
+		}
+
+		return originalEmit.apply(cp, [name, ...args]);
+	};
+}
+
+export default { notFoundError, verifyENOENT, hookChildProcess };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,41 @@
-export function fn() {
-	return "Hello, timbic";
+import cp, { type SpawnOptions } from "node:child_process";
+import utils from "./utils.ts";
+import parse from "./parse.ts";
+import enoent from "./enoent.ts";
+
+/**
+ * The `spawn()` function spawns a new process using the given `command`, with
+ * command line arguments in `args`. If omitted, `args` defaults to an empty array.
+ */
+function spawn(command: string, args: ReadonlyArray<string> = [], options: SpawnOptions = {}) {
+	// Parse the arguments
+	const parsed = parse(command, args, options);
+
+	// Spawn the child process
+	const spawned = cp.spawn(parsed.command, parsed.args, parsed.options);
+
+	// Hook into child process "exit" event to emit an error if the command
+	// does not exists, see: https://github.com/IndigoUnited/node-cross-spawn/issues/16
+	enoent.hookChildProcess(spawned, parsed);
+
+	return spawned;
 }
+
+/**
+ * The `sync()` function spawns a new process using the given `command`, with
+ * command line arguments in `args`. If omitted, `args` defaults to an empty array.
+ */
+function sync(command: string, args: ReadonlyArray<string> = [], options: SpawnOptions = {}) {
+	// Parse the arguments
+	const parsed = parse(command, args, options);
+
+	// Spawn the child process
+	const result = cp.spawnSync(parsed.command, parsed.args, parsed.options);
+
+	// Analyze if the command does not exist, see: https://github.com/IndigoUnited/node-cross-spawn/issues/16
+	result.error = result.error || enoent.verifyENOENT(result.status, parsed, "spawnSync") || undefined;
+
+	return result;
+}
+
+export { sync, spawn, parse as _parse, enoent as _enoent, utils as _utils };
