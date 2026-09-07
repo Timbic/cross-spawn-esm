3c63095d6e66220205a53df4f59d9e8520e9209d
Тимур Бикмухаметов <tim.bic22@gmail.com>
2026-09-07 19:42:14 +0300

    chore: finish documenting path-key.ts

--- src/utils/path-key.ts
+++ src/utils/path-key.ts
@@ -1,37 +1,39 @@
-import { isWin, env } from "./variables";
+import { isWin, _env } from "./variables";
 
 export interface PathKeyOptions {
 	/**
-	Use a custom environment variables object.
-
-	Default: [`process.env`](https://nodejs.org/api/process.html#process_process_env).
-	*/
+	 * Use a custom environment variables object.
+	 * @default process.env
+	 *
+	 * @see https://nodejs.org/api/process.html#process_process_env
+	 */
 	env?: Record<string, string | undefined>;
 	/**
-	Get the PATH key for a specific platform.
-
-	Default: [`process.platform`](https://nodejs.org/api/process.html#process_process_platform).
-	*/
+	 * Get the PATH key for a specific platform.
+	 * @default process.platform
+	 *
+	 * @see https://nodejs.org/api/process.html#process_process_platform
+	 */
 	platform?: NodeJS.Platform;
 }
 
 /**
-Get the [PATH](https://en.wikipedia.org/wiki/PATH_(variable)) environment variable key cross-platform.
-
-@example
-```
-const key = pathKey();
-//=> 'PATH'
-
-const PATH = process.env[key];
-//=> '/usr/local/bin:/usr/bin:/bin'
-```
-*/
+ * Get the [PATH] environment variable key cross-platform.
+ * @see https://en.wikipedia.org/wiki/PATH_(variable)
+ *
+ * @example
+ * ```
+ * const key = pathKey();
+ * //=> 'PATH'
+ *
+ * const PATH = process.env[key];
+ * //=> '/usr/local/bin:/usr/bin:/bin'
+ */
 export function pathKey(options?: PathKeyOptions) {
 	if (options?.platform ?? isWin) return "PATH";
 
 	return (
-		Object.keys(options?.env ?? env)
+		Object.keys(options?.env ?? _env)
 			.reverse()
 			.find((key) => key.toUpperCase() === "PATH") || "Path"
 	);
